71058102d78a55421436e1a98a8a25edab12635e
Marco Pontili <1044204+marcop135@users.noreply.github.com>
2021-01-27 10:50:20 +0000

    chore: bumpup semver

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ function scssTask() {
       .pipe(
         replace(
           '@charset "UTF-8";',
-          "/*! bullframe.css v3.4.1 | MIT License | https://github.com/marcop135/bullframe.css */"
+          "/*! bullframe.css v3.4.2 | MIT License | https://github.com/marcop135/bullframe.css */"
         )
       )
 

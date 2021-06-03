9e6255142e10ff5ddf8620811f8d4c4a63b0fc22
Marco Pontili <marcop135.github@gmail.com>
2021-06-03 11:01:56 +0100

    [CHO] Bump up version to 3.8.0

--- gulpfile.js
+++ gulpfile.js
@@ -55,7 +55,7 @@ function scssTask() {
       .pipe(
         replace(
           '@charset "UTF-8";',
-          '/*! bullframe.css v3.7.1 | MIT License | https://github.com/marcop135/bullframe.css */'
+          '/*! bullframe.css v3.8.0 | MIT License | https://github.com/marcop135/bullframe.css */'
         )
       )
 

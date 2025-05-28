5f619b756f5bf2bd062598ecfb76f63c3e1005a1
marcop135 <marcop135.github@gmail.com>
2025-05-28 14:18:08 +0200

    enh: add modern prettier config file (JSDoc)

---
+++ prettier.config.js
@@ -0,0 +1,16 @@
+/** @type {import('prettier').Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf"
+};

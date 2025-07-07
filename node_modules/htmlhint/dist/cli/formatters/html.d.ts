fdb8ad371778e0c1cd3a8a306be0336f937d1f5a
marcop135 <marcop135.github@gmail.com>
2025-07-07 20:18:12 +0200

    fix: remove node_modules/

--- node_modules/htmlhint/dist/cli/formatters/html.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=html.d.ts.map
--- node_modules/htmlhint/dist/core/types.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvY29yZS90eXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=

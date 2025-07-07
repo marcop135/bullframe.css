dd89762c77631e1dbb5180312d182acee257fd1e
marcop135 <marcop135.github@gmail.com>
2025-07-07 17:38:33 +0200

    chore: allow html and md long lines

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,26 @@
 /** @type {import('prettier').Config} */
 export default {
-  "semi": true,
-  "singleQuote": true,
-  "jsxSingleQuote": true,
-  "tabWidth": 2,
-  "useTabs": false,
-  "printWidth": 98,
-  "trailingComma": "es5",
-  "bracketSpacing": true,
-  "bracketSameLine": false,
-  "arrowParens": "always",
-  "proseWrap": "preserve",
-  "htmlWhitespaceSensitivity": "css",
-  "endOfLine": "lf"
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 98,
+  trailingComma: "es5",
+  bracketSpacing: true,
+  bracketSameLine: false,
+  arrowParens: "always",
+  proseWrap: "preserve",
+  htmlWhitespaceSensitivity: "css",
+  endOfLine: "lf",
+  overrides: [
+    {
+      files: "*.html",
+      options: { printWidth: 9999 },
+    },
+    {
+      files: "*.md",
+      options: { printWidth: 9999 },
+    },
+  ],
 };
